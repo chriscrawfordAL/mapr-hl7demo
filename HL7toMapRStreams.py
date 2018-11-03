46411b36c41b3dd24766cc11d0fe0d0e46c445c2
crawdaddy18 <crawfordx@gmail.com>
2018-11-02 22:02:48 -0400

    commit of test script and MapR streams setup

---
+++ HL7toMapRStreams.py
@@ -0,0 +1,23 @@
+import time
+import json
+
+from confluent_kafka import Producer
+from hl7_to_dict import hl7_str_to_dict
+
+#Set up my producer
+p = Producer({'streams.producer.default.stream': '/hl7stream'})
+
+str_msg=''
+with open("input.txt") as f:
+    for line in f:
+        if line!='\n':
+            str_msg=str_msg+line
+        else:
+            print(str_msg)
+            d = hl7_str_to_dict(str_msg)
+
+            p.produce('topic1', str_msg)
+            # Or - just do a json.dumps(your_json) instead of str_msg
+            p.flush()
+            str_msg=''
+            time.sleep(10)
---
+++ README.md
@@ -0,0 +1,7 @@
+
+----Install hl7apy
+sudo pip install hl7apy
+copy hl7_to_dict.py to your Library/Python/2.7/site-packages (or equivalent)
+
+run test_outputToStdout.py to test install
+
